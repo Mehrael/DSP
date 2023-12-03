a8e8aff484ba2056e20b58dc4e9282d96d6faa2e
Monica Sameh Salib <monica.sameh.salib@gmail.com>
2023-12-03 22:33:44 +0200

    Not finished Convolution task :(

--- Signals/Task6/Convolution/ConvTest.py
+++ Signals/Task6/Convolution/ConvTest.py
@@ -1,4 +1,4 @@
-def ConvTest(Your_indices,Your_samples): 
+def ConvTest(Your_indices, Your_samples):
     """
     Test inputs
     InputIndicesSignal1 =[-2, -1, 0, 1]
@@ -7,22 +7,22 @@ def ConvTest(Your_indices,Your_samples):
     InputIndicesSignal2=[0, 1, 2, 3, 4, 5 ]
     InputSamplesSignal2 = [ 1, -1, 0, 0, 1, 1 ]
     """
-    
-    expected_indices=[-2, -1, 0, 1, 2, 3, 4, 5, 6]
-    expected_samples = [1, 1, -1, 0, 0, 3, 3, 2, 1 ]
 
-    
-    if (len(expected_samples)!=len(Your_samples)) and (len(expected_indices)!=len(Your_indices)):
+    expected_indices = [-2, -1, 0, 1, 2, 3, 4, 5, 6]
+    expected_samples = [1, 1, -1, 0, 0, 3, 3, 2, 1]
+    # expected_indices =[0,1,2,3,4,5,6,7,8]
+    # expected_samples=[6,11,15,18,14,10,6,3,1]
+    if (len(expected_samples) != len(Your_samples)) and (len(expected_indices) != len(Your_indices)):
         print("Conv Test case failed, your signal have different length from the expected one")
         return
     for i in range(len(Your_indices)):
-        if(Your_indices[i]!=expected_indices[i]):
-            print("Conv Test case failed, your signal have different indicies from the expected one") 
+        if (Your_indices[i] != expected_indices[i]):
+            print("Conv Test case failed, your signal have different indicies from the expected one")
             return
     for i in range(len(expected_samples)):
         if abs(Your_samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print("Conv Test case failed, your signal have different values from the expected one") 
+            print("Conv Test case failed, your signal have different values from the expected one")
             return
     print("Conv Test case passed successfully")
--- Task7.py
+++ Task7.py
@@ -5,11 +5,13 @@ from tkinter.ttk import *
 import tkinter.messagebox as msgbx
 from ImpFunctions import *
 import numpy as np
+from Signals.Task6.Convolution.ConvTest import *
 
 signal_index_1 = []
 signal_index_2 = []
 signal_samples_1 = []
 signal_samples_2 = []
+
 
 def Task7_screen():
     root = Toplevel()
@@ -20,16 +22,58 @@ def Task7_screen():
 
     Button(root, text='Signal 2', command=lambda: setSignal(2)).pack(padx=10, pady=10)
 
-    Button(root, text='Calculate',command=lambda: convolution()).pack(padx=10, pady=10)
+    Button(root, text='Calculate', command=lambda: convolution()).pack(padx=10, pady=10)
+
 
 def setSignal(x):
     if x == 1:
-        signal_index_1[:],signal_samples_1[:] = open_file()
+        signal_index_1[:], signal_samples_1[:] = open_file()
     else:
-        signal_index_2[:],signal_samples_2[:] = open_file()
+        signal_index_2[:], signal_samples_2[:] = open_file()
+
+
+def get_boundaries():
+    min_bound = min(signal_index_1) + min(signal_index_2)
+    max_bound = max(signal_index_1) + max(signal_index_2)
+    return min_bound, max_bound
+
 
 def convolution():
-    print("inedx 1:",signal_index_1)
-    print("samples 1:",signal_samples_1)
-    print("inedx 2:",signal_index_2)
-    print("samples 2:",signal_samples_2)
+    conv_index = []
+    conv_sample = []
+    start, end = get_boundaries()
+    for n in range(start, end + 1):
+        # lw bazet heya de el sabab
+        acc = 0
+        for k in range(min(signal_index_1), max(signal_index_1)):
+            if k not in signal_index_1 or (n - k) not in signal_index_2:
+                print('-------------------------')
+                print('Accumulator:', acc)
+                print('k:', k)
+                print('n:', n)
+                break
+            else:
+                i1 = signal_index_1.index(k)
+                i2 = signal_index_2.index(n - k)
+                acc += signal_samples_1[i1] * signal_samples_2[i2]
+
+                print('Indices:', i1, '  ', i2)
+
+        conv_index.append(n)
+        conv_sample.append(acc)
+    ConvTest(conv_index, conv_sample)
+    print('Index:', conv_index)
+    print('Sample:', conv_sample)
+    # len_x = len(signal_samples_1)
+    # len_h = len(signal_samples_2)
+    # conv_result = [0] * (len_x + len_h - 1)
+    #
+    # for n in range(start, end+1):
+    #     for k in range(max(0, n - len_h + 1), min(len_x, n + 1)):
+    #         conv_result[n] += signal_samples_1[k] * signal_samples_2[n - k]
+    #     conv_index.append(n)
+    # conv_sample[:]=conv_result
+    # print('Indices: ', conv_index)
+    # print('Sample: ', conv_sample)
+    # ConvTest(conv_index,conv_sample)
+    return
