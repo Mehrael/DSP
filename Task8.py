4325fd4825797bb5ae4ca69937dc8d4247d2a272
Monica Sameh Salib <monica.sameh.salib@gmail.com>
2023-12-09 21:43:51 +0200

    Finished Task 8 :)

--- Task8.py
+++ Task8.py
@@ -1,5 +1,6 @@
 import math
 from ImpFunctions import *
+from Signals.Task8.CompareSignal import *
 
 
 def denominator(N, s1, s2):
@@ -16,3 +17,17 @@ def denominator(N, s1, s2):
 def correlation():
     index1, signal1 = open_file('Signals/Task8/Corr_input signal1.txt')
     index2, signal2 = open_file('Signals/Task8/Corr_input signal2.txt')
+    den = denominator(len(signal1), signal1, signal2)
+    corr = []
+    index = []
+    for i in range(len(signal1)):
+        acc = 0
+        for j in range(len(signal2)):
+            acc += signal1[j] * signal2[j]
+        corr.append((acc / len(signal1)) / den)
+        temp = signal2[0]
+        signal2.pop(0)
+        signal2.append(temp)
+        index.append(i)
+    Compare_Signals('Signals/Task8/CorrOutput.txt', index, corr)
+    return
