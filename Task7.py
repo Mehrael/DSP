63c0c7877741122d970e336d2369d885dbe2252c
Monica Sameh Salib <monica.sameh.salib@gmail.com>
2023-12-09 21:05:34 +0200

    Added Task 8 Files

--- Task7.py
+++ Task7.py
@@ -1,7 +1,8 @@
 from ImpFunctions import *
 from Signals.Task6.Convolution.ConvTest import *
 
-def get_boundaries(signal_index_1,signal_index_2):
+
+def get_boundaries(signal_index_1, signal_index_2):
     min_bound = min(signal_index_1) + min(signal_index_2)
     max_bound = max(signal_index_1) + max(signal_index_2)
     return min_bound, max_bound
@@ -12,10 +13,10 @@ def convolution():
     signal_index_2, signal_samples_2 = open_file('Signals/Task6/Convolution/Input_conv_Sig2.txt')
     conv_index = []
     conv_sample = []
-    start, end = get_boundaries(signal_index_1,signal_index_2)
+    start, end = get_boundaries(signal_index_1, signal_index_2)
     for n in range(start, end + 1):
         acc = 0
-        for k in range(min(signal_index_1), end+1):
+        for k in range(min(signal_index_1), end + 1):
             if k not in signal_index_1:
                 break
             elif (n - k) not in signal_index_2:
