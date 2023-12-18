1bb2719804d690d7095cbaf332f708eb14d2ea8d
Mehrael <mehraelashraf20@gmail.com>
2023-12-18 14:55:42 +0200

    Fast Convolution is DONE, Restored old convolution function, remove all prints, correct the padding, new DFT & IDFT

--- Task9.py
+++ Task9.py
@@ -1,76 +1,47 @@
-import math
-import tkinter
-from tkinter import *
-from tkinter.ttk import *
-import tkinter.messagebox as msgbx
-from ImpFunctions import *
 import numpy as np
-import warnings
-from collections import OrderedDict
 from Task7 import *
 from Signals.Task9.Convolution.ConvTest import ConvTest
 
 def fast_convolution():
-    from Task4 import I_DFT as DFT_IDFT
     index1, sample1 = open_file("Signals/Task9/Convolution/Input_conv_Sig1.txt")
     index2, sample2 = open_file("Signals/Task9/Convolution/Input_conv_Sig2.txt")
-
-    print("Index 1: ",index1)
-    print("Sample 1: ",sample1)
-    print()
-    print("Index 2: ",index2)
-    print("Sample 2: ",sample2)
-    print()
 
     N1 = len(sample1)
     N2 = len(sample2)
 
-    padding_val = N1 + N2 - 1
+    N = N1 + N2 - 1
 
-    index1, sample1= padding(padding_val, N1,index1,sample1)
-    index2, sample2= padding(padding_val, N2,index2,sample2)
+    sample1 = sample1 + [0.0] * (N - N1)
+    sample2 = sample2 + [0.0] * (N - N2)
 
-    print("Padded Index 1: ", index1)
-    print("Padded Sample 1: ", sample1)
-    print()
-    print("Padded Index 2: ", index2)
-    print("Padded Sample 2: ", sample2)
-    print()
+    sample1 = DFT(sample1)
+    sample2 = DFT(sample2)
 
-    amp1, phase1 = DFT_IDFT(sample1,0,False,[],[])
-    amp2, phase2 = DFT_IDFT(sample2,0,False,[],[])
+    conv_signal = sample1 * sample2
 
-    print("Amp 1: ", amp1)
-    print("Phase 1: ", phase1)
-    print()
-    print("Amp 2: ", amp2)
-    print("Phase 2: ", phase2)
-    print()
+    result = IDFT(conv_signal)
 
-    conv_index_amp, conv_amp = convolution(index1,amp1,index2,amp2)
-    conv_index_phase, conv_phase = convolution(index1,phase1,index2,phase2)
+    ConvTest(index1,result)
 
-    print("Conv Amp: ", conv_amp)
-    print()
-    print("Conv Phase: ", conv_phase)
-    print()
+def DFT(x):
+    N = len(x)
+    sample = np.zeros(N, dtype=np.complex128)
 
-    restored_index, restored_sample = DFT_IDFT([],0,True,conv_amp,conv_phase)
+    for n in range(N):
+        for k in range(N):
+            sample[n] += x[k] * np.exp(-2j * np.pi * k * n / N)
 
-    print("Restored Index",restored_index)
-    print("Restored Sample",restored_sample)
-    print()
+    return sample
 
-    ConvTest(restored_index,restored_sample)
+def IDFT(x):
+    N = len(x)
+    sample = np.zeros(N)
 
-def padding(N, lst_size, index, sample):
+    for n in range(N):
+        for k in range(N):
+            sample[n] += x[k] * np.exp(2j * np.pi * k * n / N)
+        sample[n] = sample[n] / N
 
-    for i in range(lst_size, N):
-        index.append(index[-1] + 1)
-
-    sample = sample + [0.0] * (N - lst_size)
-
-    return index, sample
-
+    return sample
 def fast_correlation():
     return
